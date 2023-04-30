cd5c41d21a57daf91893077c504423db1b3bbd2f
Damian Roiz <damianx989@gmail.com>
2023-04-30 11:07:26 -0400

    exercises 1.1 to 1.5

---
+++ part-1/course-info/src/App.js
@@ -0,0 +1,65 @@
+import React from "react";
+
+const Header = (props) => {
+  return <h1>{props.course}</h1>;
+};
+
+const Part = (props) => {
+  return (
+    <div>
+      <p>{props.subject.name}</p>
+      <p>Number of exercises:{props.subject.exercises}</p>
+    </div>
+  );
+};
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part subject={props.subject[0]} />
+      <Part subject={props.subject[1]} />
+      <Part subject={props.subject[2]} />
+    </div>
+  );
+};
+
+const Total = (props) => {
+  return (
+    <p>
+      Total number of exercises:{" "}
+      {props.exercises[0].exercises +
+        props.exercises[1].exercises +
+        props.exercises[2].exercises}
+    </p>
+  );
+};
+
+const App = () => {
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
+
+  return (
+    <div className="app">
+      <Header course={course.name} />
+      <Content subject={course.parts} />
+      <Total exercises={course.parts} />
+    </div>
+  );
+};
+
+export default App;
