b3162ff79c9d2db23e4a6eb9f47d4d04402f28ca
Damian Roiz <damianx989@gmail.com>
2023-09-10 12:56:57 -0400

    3.8 complete

--- part-3/phonebook-backend/index.js
+++ part-3/phonebook-backend/index.js
@@ -1,7 +1,20 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 
 app.use(express.json());
+// app.use(morgan("combined"));
+
+morgan.token("body", (req, res) => JSON.stringify(req.body));
+
+app.use(
+  morgan(
+    ":method :url :status - :res[content-length] - :response-time ms - :body", // Include the "request-body" token in the log format
+    {
+      skip: (req) => req.method !== "POST",
+    }
+  )
+);
 
 //////////// 3.1
 let persons = [
@@ -27,29 +40,66 @@ let persons = [
   },
 ];
 
-
 //////////// 3.1
 app.get("/api/persons", (request, response) => {
-    response.json(persons);
+  response.json(persons);
 });
 
 ////////// 3.2
-app.get('/info', (request, response) => {
-    response.send(`<p>Phonebook has info for ${persons.length} people</p><p>${date = new Date()}</p>`)
-})
+app.get("/info", (request, response) => {
+  response.send(
+    `<p>Phonebook has info for ${persons.length} people</p><p>${(date =
+      new Date())}</p>`
+  );
+});
 
-////////////// 3.3 
-app.get('/api/persons/:id', (request, response) => {
-    const id = +request.params.id
-    const person = persons.find(person => person.id === id)
+////////////// 3.3
+app.get("/api/persons/:id", (request, response) => {
+  const id = +request.params.id;
+  const person = persons.find((person) => person.id === id);
 
-    if(person) {
-        response.json(person)
-    } else {
-        response.status(404).end()
-    }
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
 
-})
+/////////////////////// 3.4
+app.delete("/api/persons/:id", (request, response) => {
+  const id = +request.params.id;
+  persons = persons.filter((p) => p.id !== id);
+
+  response.status(204).end();
+});
+
+//////////////////////////
+////// 3.8
+
+const idGenerator = () => {
+  const maxId = persons.length > 0 ? Math.max(...persons.map((p) => p.id)) : 0;
+  return maxId + 1;
+};
+
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: "content missing",
+    });
+  }
+
+  const person = {
+    id: idGenerator(),
+    name: body.name,
+    number: body.number,
+  };
+
+  persons = persons.concat(person);
+
+  response.json(person);
+});
 
 const PORT = 3001;
 app.listen(PORT, () => {
