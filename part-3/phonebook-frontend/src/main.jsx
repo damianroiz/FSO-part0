9e08032c84e5f49e7116bd8083b2259b8b38b874
Damian Roiz <damianx989@gmail.com>
2023-12-09 14:45:30 -0500

    Updated imports  in main.jsx

--- part-3/phonebook-frontend/src/main.jsx
+++ part-3/phonebook-frontend/src/main.jsx
@@ -1,15 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import App from "./App";
 import "./index.css";
-import axios from "axios";
 
-const promise = axios.get("http://localhost:3005/persons");
-console.log(promise);
-
+// const promise = axios.get("http://localhost:3001/persons");
+// console.log(promise);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <App />
-  </React.StrictMode>
+  // </React.StrictMode>
 );
