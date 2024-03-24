ab2ef861d3be1a407c7f4cf4a8f31dab8cd9c688
Damian Roiz <damianx989@gmail.com>
2024-03-24 18:25:09 -0400

    4.9 complete

--- part-4/bloglist-bend/tests/blog_api.test.js
+++ part-4/bloglist-bend/tests/blog_api.test.js
@@ -1,14 +1,26 @@
-const {test, after } = require('node:test')
-const assert = require("node:assert")
-const mongoose = require('mongoose')
-const supertest = require('supertest')
-const app = require('../app')
+const { test, after } = require("node:test");
+const assert = require("node:assert");
+const mongoose = require("mongoose");
+const supertest = require("supertest");
+const app = require("../app");
 
-const api = supertest(app)
+const api = supertest(app);
 
 /// 4.8
-test('return the number of blogs', async () => {
-    const response = await api.get('/api/blogs')
-    assert(response.body.length, 1)
+test("return the number of blogs", async () => {
+  const response = await api.get("/api/blogs");
+  assert(response.body.length, 1);
+});
 
-})
+//4.9
+test("the id property exists", async () => {
+  const response = await api.get("/api/blogs");
+
+  response.body.forEach((blog) => {
+    assert(blog.id);
+  });
+});
+
+after(async () => {
+  await mongoose.connection.close();
+});
